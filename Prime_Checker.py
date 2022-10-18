db0604104fb0b1b78b8b56af05f68a6b3a4d0440
Cokaine2912 <97113663+Cokaine2912@users.noreply.github.com>
2022-10-18 21:57:40 +0530

    Add files via upload
    Python code for checking a number whether it is Prime or Not.

---
+++ Prime_Checker.py
@@ -0,0 +1,15 @@
+def prime_checker(n) :
+  n  = int(input())
+  d = []
+  for i in range(n-1,2,-1) :
+    d.append(a%i)
+  if 0 not in d :
+    print("IT IS A PRIME")  
+  else :
+    print("NOT PRIME")
+
+
+
+
+
+
